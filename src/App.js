a7adc15e7c33cbabb7b8088eeeccc75aa6eeda1a
pauza <pzedillo@ph.com.mx>
2021-02-09 22:43:06 -0600

    Comienza proyecto y hace fetch a la data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import DirectoryList from "ui/components/DirectoryList";
+import DirectoryList from "./ui/components/DirectoryList";
 
 const fetchOffersList = () => {
+    // Hago la petición a la API
+    fetch(
+        "https://e6di35qzm7.execute-api.us-west-2.amazonaws.com/latest/directory",
+    )
+        .then(response => response.json())
+        .then(data => console.log(data));
+
     return [];
 };
 
 export default function App() {
     return (
-        <>
-            <p class="py-4 text-lg text-center">Start here :) Good luck!</p>
+        <div>
+            <p class="py-4 text-lg text-center">Great!</p>
 
             <DirectoryList offerList={fetchOffersList()} />
-        </>
+        </div>
     );
 }
