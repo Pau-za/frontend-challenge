796bf0259be082f493b12d9131f87ea22752c3b1
pauza <pzedillo@ph.com.mx>
2021-02-10 22:40:54 -0600

    Extrae informacion especifica de la data pero aun no la lee el componente directorylist

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import DirectoryList from "./ui/components/DirectoryList";
 
 const fetchOffersList = () => {
-    // Hago la petición a la API
+    // I do the fetch to the data
     fetch(
         "https://e6di35qzm7.execute-api.us-west-2.amazonaws.com/latest/directory",
     )
         .then(response => response.json())
-        .then(data => console.log(data));
-
+        .then(data => {
+            // console.log(data.data);
+            let restaurant;
+            let discount;
+            data.data.forEach(element => {
+                for (const key in element) {
+                    restaurant = element.name;
+                    discount = element.discount;
+                    console.log(restaurant, discount);
+                }
+                DirectoryList(restaurant, discount);
+            });
+        });
     return [];
 };
 
 export default function App() {
     return (
         <div>
-            <p class="py-4 text-lg text-center">Great!</p>
-
+            <section className=" bg-gray-100 flex-1 justify-center h-96">
+                <h2 class="py-4 text-4xl text-center ">
+                    Know the offers our partners got for you!
+                </h2>
+            </section>
             <DirectoryList offerList={fetchOffersList()} />
+            {/* <DirectoryList offerList={setRestaurant} /> */}
         </div>
     );
 }
